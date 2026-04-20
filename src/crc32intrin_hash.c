64b34a330454da93b548d35016238051aba17052
MartAlx-RT <martynovalexvit99@gmail.com>
2026-04-20 22:10:45 +0300

    inline asm implemented

--- src/crc32intrin_hash.c
+++ src/crc32intrin_hash.c
@@ -2,14 +2,19 @@
 #include <x86intrin.h>
 #include <stdint.h>
 
-tbl_hash_t tbl_hash(const tbl_key_t key)
+tbl_hash_t tbl_hash(const tbl_key_t key)	// TODO rewrite on asm
 {
-	assert(key);
+	assert(key);		// TODO test unrolling
 
 	uint32_t hash = 0;
 
-	size_t i = 0;
-	while(key[i])	hash = _mm_crc32_u8(hash, key[i++]);
+	size_t i = 0;		// TODO use expanded crc
+	//while(key[i])	hash = _mm_crc32_u8(hash, key[i++]);
+	while(key[i]) {
+		uint8_t k = key[i];
+		asm volatile("crc32	%1, %0" : "=r" (hash) : "r" (k), "r" (hash));
+		i++;
+	}
 
 	return hash;
 }
