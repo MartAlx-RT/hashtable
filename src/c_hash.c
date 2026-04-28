c48dd0d992b29352f4fa40818585a6dee0ccbb3e
MartAlx-RT <martynovalexvit99@gmail.com>
2026-04-28 19:10:41 +0300

    fixed repo

--- src/c_hash.c
+++
@@ -1,9 +0,0 @@
-#include "table.h"
-#include <stdint.h>
-
-tbl_hash_t tbl_hash(const tbl_key_t key)
-{
-	assert(key);
-
-	return (uint8_t)key[0];
-}
--- src/const_hash.c
+++
@@ -1,7 +0,0 @@
-#include "table.h"
-#include <stdint.h>
-
-tbl_hash_t tbl_hash(const tbl_key_t key __attribute__((unused)))
-{
-	return 0;
-}
